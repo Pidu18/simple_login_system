6e82da41b55c90e1807bc7a1ee7e00015863107c
Pidu18 <38082683+Pidu18@users.noreply.github.com>
2018-04-05 14:47:47 +0300

    Update signup.php

--- inc/codes/signup.php
+++ inc/codes/signup.php
@@ -4,16 +4,18 @@ include '../../config/conectare.php';
 $nume = htmlspecialchars($_POST['nume']);
 $prenume = htmlspecialchars($_POST['prenume']);
 $email = htmlspecialchars($_POST['email']);
-$parola = htmlspecialchars($_POST['parola']);
+$parola = $_POST['parola'];
 
 $sql = "SELECT * FROM users WHERE email = '$email'";
 $rez = mysqli_query($conectare, $sql);
 $check = $rez->fetch_assoc();
 
-if ($check >= 1) {
+$encpw = password_hash($parola,PASSWORD_DEFAULT);
+
+if ($check == 1) {
 	header("Location: ../../index.php?o=used");
 }else{
-	$sql = "INSERT INTO users (nume, prenume, email, parola) VALUES ('$nume', '$prenume', '$email', '$parola')";
+	$sql = "INSERT INTO users (nume, prenume, email, parola) VALUES ('$nume', '$prenume', '$email', '$encpw')";
 	$rez = mysqli_query($conectare, $sql);
 	header("Location: ../../index.php?login");
 }
