abf07f6f867ea42b24ecfa3b1ac08ceafce7a371
Pidu18 <38082683+Pidu18@users.noreply.github.com>
2018-04-05 14:46:34 +0300

    Update login.php

--- inc/codes/login.php
+++ inc/codes/login.php
@@ -2,15 +2,24 @@
 include '../../config/conectare.php';
 session_start();
 
-
-$email = htmlspecialchars($_POST['email']);
-$parola = htmlspecialchars($_POST['parola']);
-$sql = "SELECT * FROM users WHERE email = '$email' AND parola = '$parola'";
+$email = $_POST['email'];
+$parola = $_POST['parola'];
+$sql = "SELECT * FROM users where email = '$email'";
 $rez = mysqli_query($conectare, $sql);
 $row = $rez->fetch_assoc();
-if ($row > 1) {
+$pw = $row['parola'];
+$ver = password_verify($parola, $pw);
+if ($ver == 0) {
+	header("Location: ../../index.php?e=invalid");
+} else {
+
+	$sql = "SELECT * FROM users WHERE email='$email' AND parola='$pw'";
+	$result = mysqli_query($conectare, $sql);
+
+	if (!$row = $result->fetch_assoc()) {
+		echo 'Parola sau usernameul nu se potriveste.';
+	}else{
 	$_SESSION['id'] = $row['id'];
 	header("Location: ../../index.php");
-}else{
-    header("Location: ../../index.php?o=failed");
+    }
 }
